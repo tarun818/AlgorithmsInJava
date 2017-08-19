103f3997cdf537890b1879e19f025c96d0819ce4
tarun818 <tarun818@gmail.com>
2017-08-19 15:09:59 +0530

    Added some minor details in comment.

--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 
 public class SelectionSort
 {
+/*	The selection sort algorithm sorts an array by repeatedly finding the minimum element 
+	(considering ascending order) from unsorted part and putting it at the beginning. The algorithm maintains two subarrays in a given array.
+	1) The subarray which is already sorted.
+	2) Remaining subarray which is unsorted.
+	In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray.
+
+	Time Complexity: O(n*n) as there are two nested loops.
+	Auxiliary Space: O(1)*/
+	
 
 	public static void main(String[] args)
 	{
