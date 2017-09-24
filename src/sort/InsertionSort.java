ef8b445a09a9bbdad2eeab19e5a6911224187749
tarun818 <tarun818@gmail.com>
2017-09-24 21:35:24 +0530

    Insertion sort impimented.

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -24,11 +24,33 @@ public class InsertionSort
 			System.out.println("Please enter an integer value for index "+i+": ");
 			unsortedArray[i]=inputscanner.nextInt();
 		}
+		inputscanner.close();
 		System.out.println("Unsorted array :"+Arrays.toString(unsortedArray));
 		
+		insertSort(unsortedArray);
 		
-		
+		System.out.println("Sorted array after insertion sort :"+Arrays.toString(unsortedArray));
 
 	}
 
+    /**
+     * @param unsortedArray
+     */
+    private static void insertSort(int[] unsortedArray) 
+    {
+        //Started from i=1 because we divide array in imaginary two parts one is sorted another is unsorted.
+        //and single element is always in sorted form 
+        for(int i=1;i<unsortedArray.length;++i)
+        {
+            for(int j = i ; j > 0 ; j--)
+            {
+                if(unsortedArray[j] < unsortedArray[j-1])
+                {
+                    int temp = unsortedArray[j];
+                    unsortedArray[j] = unsortedArray[j-1];
+                    unsortedArray[j-1] = temp;
+                }
+            }
+        }
+    }
 }
